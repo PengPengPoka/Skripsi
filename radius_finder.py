8787b9ca813ee2c1554f837a99dfd99c18e1cb88
PengPengPoka <hafizhfanani03@gmail.com>
2025-02-25 22:52:41 +0700

    add blur test

--- radius_finder.py
+++ radius_finder.py
@@ -2,13 +2,12 @@ import cv2 as cv
 import numpy as np
 import os
 import csv
-from sympy.physics.wigner import wigner_d
-from urllib3.filepost import writer
 
-
+# buat trackbar
 def on_trackbar():
     pass
 
+# to crop the image circularly
 def crop_trackbar(height, width, px, py, radius, src):
     canvas = np.zeros((height, width, 3), dtype=np.uint8)
     img = src.copy()
@@ -24,11 +23,12 @@ def crop_trackbar(height, width, px, py, radius, src):
 def main():
     # directory location
     home = os.getcwd()
-    variants = ["DUST", "DUST2", "DUST3", "BOHEA", "BOP", "BOPF", "BOPF1", "F1", "F2", "PF", "PF2", "PF3", "BP"]
-    img_path = home + "\\Data_21-09-2023[1]\\Warna\\" + variants[0] + "_1-1.jpg"
+    # variants = ["DUST", "DUST2", "DUST3", "BOHEA", "BOP", "BOPF", "BOPF1", "F1", "F2", "PF", "PF2", "PF3", "BP"]
+    # img_path = home + "\\Data_21-09-2023[1]\\Warna\\" + variants[0] + "_1-1.jpg"
+    img_path = home + "//Data_NEW//BOHEA//BOHEA_005.jpg"
     img = cv.imread(img_path) # read image
 
-    if img is None:
+    if img is None:         # check if image exist
         print(img_path)
         print("no image detected")
 
@@ -44,19 +44,21 @@ def main():
         cv.createTrackbar("y space", "crop trackbar", 0, height - 1, on_trackbar)
         cv.createTrackbar("radius", "crop trackbar", 0, 450, on_trackbar)
 
-        cv.setTrackbarPos("x space", "crop trackbar", 329)
-        cv.setTrackbarPos("y space", "crop trackbar", 240)
-        cv.setTrackbarPos("radius", "crop trackbar", 116)
+        cv.setTrackbarPos("x space", "crop trackbar", 320)
+        cv.setTrackbarPos("y space", "crop trackbar", 232)
+        cv.setTrackbarPos("radius", "crop trackbar", 94)
 
         while True:
+            # get trackbar position
             x = cv.getTrackbarPos("x space", "crop trackbar")
             y = cv.getTrackbarPos("y space", "crop trackbar")
             radius = cv.getTrackbarPos("radius", "crop trackbar")
 
+            # draw circle unto image
             img_copy = img.copy()
             cv.circle(img_copy, (x, y), radius, [0, 0, 255], 2)
 
-            crop_img = crop_trackbar(height, width, x, y, radius, img)
+            crop_img = crop_trackbar(height, width, x, y, radius, img)      # get cropped image
             cv.imshow("crop trackbar", img_copy)
             cv.imshow("crop", crop_img)
             key = cv.waitKey(3)
@@ -70,6 +72,9 @@ def main():
                     writer.writerow(['Parameter'])
                     for item in data:
                         writer.writerow([item])
+            elif key == ord('i'):
+                filename = input("insert filename: ")
+                cv.imwrite((filename + ".jpg"), crop_img)
 
 
 if __name__ == "__main__":
