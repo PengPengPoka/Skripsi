77bb1eb4eb4a6b8d71cd6dbdfaf52486b4b4242a
PengPengPoka <hafizhfanani03@gmail.com>
2025-08-14 19:32:22 +0700

    resolve conflict with thinkpad

--- cropper.py
+++ cropper.py
@@ -1,32 +1,3 @@
-<<<<<<< HEAD
-import cv2 as cv
-import os
-from matplotlib import pyplot as plt
-
-def main():
-    directory = os.path.expanduser('~')
-    tea_class = ("DUST", "DUST2", "DUST3", "BOHEA", "BOP", "BOPF", "BOPF1", "FI", "F2", "PF", "PF2", "PF3", "BP")
-    
-    i = 1
-    j = 1
-    
-    tea_file = directory + "\\Repositories\\Skripsi\\Data_NEW\\" + tea_class[0] + "\\DUST_001.jpg"
-    print(tea_file)
-    
-    image = cv.imread(tea_file)
-    
-    if image is None:
-        print("No image")
-    
-    cv.imshow("DUST", image)
-    key = cv.waitKey()
-    
-    if key == ord('q'):
-        exit()
-    
-if __name__ == "__main__":
-    main()
-=======
 """
 Cropping image data of tea using mask
 """
@@ -113,4 +84,3 @@ def main():
 
 
 main()
->>>>>>> 73a5eb45009234deda7c83d209d972e317a29406
