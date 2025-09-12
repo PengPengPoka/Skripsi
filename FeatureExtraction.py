c6cb29f905c90e0c4760cc7a616c3d6e00d434c4
PengPengPoka <hafizhfanani03@gmail.com>
2025-09-12 14:25:37 +0700

    p

--- FeatureExtraction.py
+++ FeatureExtraction.py
@@ -1,33 +1,19 @@
-"""
-Feature extraction from RGB, HSV, and LAB images
-Splitting the images into individual channel to extract the data
-"""
-
 import cv2 as cv
-import os
+import numpy as np
+import pandas as pd
+from pathlib import Path
 
-def ImageColorReader(directory, variant, output_dir, extention='.jpg'):
-    """
-    args:
-        directory   : directory containing the files of the tea images
-        variant     : variant of tea
-        output_dir  : output directory to store the processed images
-        extention   : file format for the image, defaulted to jpg
-    """
-    os.makedirs(output_dir, exist_ok=True)
-    print(f"Creating {output_dir} directory")
-    
-
+def FeatureExtraction():
+    ...
 
 def main():
-    directory = os.getcwd()
-    DATASET_DIRECTORY = os.path.join(directory, "Processed Images")
-    OUTPUT_DIRECTOR = os.path.join(directory, "Colour Data")
+    BASE_DIR = Path(__file__).absolute().parent
+    PREPROCESSED_DATA_DIR = BASE_DIR / "Preprocessed Images"
     
-    TOTAL_IMAGES_PER_VARIANT = [561, 600, 300, 601, 590, 430, 599, 300, 441, 600]
+    COLOR_MODE = ['RGB', 'HSV', 'LAB']
     TEA_VARIANTS = ['BOHEA', 'BOP', 'BOPF', 'DUST', 'DUST_II', 'F_I', 'F_II', 'PF', 'PF_II', 'PF_III']
     
     
     
-    
-main()
+if __name__ == "__main__":
+    main()
